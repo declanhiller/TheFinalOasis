de138d914fbb9b07c3768c31bd04d9248008b382
Declan Hiller <declan.hiller@gmail.com>
2020-06-12 08:15:31 -0600

    Some pretty important changes I forgot to commit lol

---
+++ The Final Oasis(Unity)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moves player
+        if(Input.GetKey(KeyCode.W)) {
+            this.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+        //Faces Player to Mouse
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float midPoint = (transform.position - Camera.main.transform.position).magnitude * 0.5f;
+        transform.LookAt(mouseRay.origin + mouseRay.direction * midPoint);
+    }
+}
