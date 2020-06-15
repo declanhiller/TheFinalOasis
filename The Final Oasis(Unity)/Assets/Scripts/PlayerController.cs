6d9afa15c793eedfb79afb3e6bd690af9bbb508e
Declan Hiller <declan.hiller@gmail.com>
2020-06-14 21:31:03 -0600

    Pushing a few things I forgot to push.. coral some changes to seal anim and new files... also fixed a small pause in pufferfish animation

--- The Final Oasis(Unity)/Assets/Scripts/PlayerController.cs
+++ The Final Oasis(Unity)/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 10.0f;
+    GameObject head;
     // Start is called before the first frame update
     void Start()
     {
-        
+        head = GameObject.FindGameObjectWithTag("Head");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Moves player
-        if(Input.GetKey(KeyCode.W)) {
-            this.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
-        }
-        //Faces Player to Mouse
-        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        float midPoint = (transform.position - Camera.main.transform.position).magnitude * 0.5f;
-        transform.LookAt(mouseRay.origin + mouseRay.direction * midPoint);
+
+        float xInput = 0;
+        float yInput = 0;
+        xInput = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        yInput = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        //head.transform.rotation = Quaternion.Euler(head.transform.rotation.eulerAngles.x - 1, head.transform.rotation.eulerAngles.y + yInput, head.transform.rotation.eulerAngles.z);
+        head.transform.Rotate(new Vector3(-xInput, 0, -yInput));
     }
+
 }
